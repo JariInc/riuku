2e2361ccced5908ae6da0602d057b2e6c15bad16
Jari Ylimäinen <jari@ylimainen.fi>
2013-03-30 10:21:12 +0200

    Get also names from the imported xml

--- google-reader-import.php
+++ google-reader-import.php
@@ -1,7 +1,14 @@
 <?php 
 	include("header.inc.php");
 ?>
-	<h1>Google reader import</h1>
+	<h1>Google Reader import</h1>
+	<ol>
+		<li>Go to <a href="https://www.google.com/takeout">Google takeout</a></li>
+		<li>Download an archive of your Google Reader data</li>
+		<li>Extract subscriptions.xml from the archive</li>
+		<li>Upload subscriptions.xml using the form below</li>
+	</ol>
+	<h2>Import</h2>
 	<form method="post" action="google-reader-import.php" enctype="multipart/form-data">
 		<p><input type="file" name="subs"></p>
 		<p><input type="submit" class="btn btn-primary" value="Import"></p>
@@ -11,31 +18,26 @@
 	if($_FILES) {
 		$xml = simplexml_load_file($_FILES['subs']['tmp_name']);
 		
-		require_once('simplepie_loader.php');
 		require_once("db.php");
 		
 		$db = new db("db/riuku.db");
-		$feed = new SimplePie();
 		
 		echo "<h2>Feeds added</h2>";
-		echo "<ul>";
-		foreach($xml->xpath('//@xmlUrl') as $element) {
-			$url = (string)$element[0];
-			$feed->set_feed_url($url);
-			$feed->init();
-			$feed->handle_content_type();
-
-			$name = $feed->get_title();
-
-			$db->query("INSERT INTO feeds(name, url) VALUES('". sqlite_escape_string($name) ."', '". sqlite_escape_string($url) ."');");
+		echo "<ol>";
 		
-			echo "<li>". $name ."</li>";
+		foreach($xml->xpath('//outline') as $element) {
+			$attrs = (array)$element->attributes();
+			$attrs_arr = $attrs["@attributes"];
 			
-			flush();
+			$url = $attrs_arr["xmlUrl"] ;
+			$name = $attrs_arr["title"];
+				
+			if($url && $name) {
+				$db->query("INSERT INTO feeds(name, url) VALUES('". sqlite_escape_string($name) ."', '". sqlite_escape_string($url) ."');");
+				echo "<li>". $name ."</li>";
+			}
 		}
-		
-		echo "</ul>";
-
+		echo "</ol>";
 	}
 	
 	include("footer.inc.php");
